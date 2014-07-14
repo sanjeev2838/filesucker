0765567f5813a54fd5726c6999d5f8157fc620ae
Sanjeev <sanjeevimpinge@gmail.com>
2014-07-14 17:46:52 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filesucker::Application.config.secret_key_base = 'a8df6c0afa992951e25d21a69cdd9042ace7848c8bae89b31b9841b8a2ce2d977c4656433abb3cc74c0e16357f6f44a6f577d0089a901b5548e951a755243d7d'
